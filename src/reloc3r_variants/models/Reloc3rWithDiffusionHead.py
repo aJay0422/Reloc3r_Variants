e3627f86712f8b0ec5435d1bbeaa15b2413a4cd8
Shangjie Du <shangjie@sn4622126138.ucmerced.edu>
2025-08-30 06:37:14 +0000

    update pose forward and sampling

--- src/reloc3r_variants/models/Reloc3rWithDiffusionHead.py
+++ src/reloc3r_variants/models/Reloc3rWithDiffusionHead.py
@@ -31,6 +31,26 @@ def posemb_sincos(
     return pos_emb
 
 
+def compute_gt_poses(view1, view2):
+    """
+    Compute the two-way relative camera poses between two views.
+    Each pose is a vector of length 12, where the first 9 elements are flattened rotation matrix and the last 3 elements are translation vector.
+    """
+    abs_pose1, abs_pose2 = view1["camera_pose"], view2["camera_pose"]
+    gt_pose1to2 = torch.inverse(view2['camera_pose']) @ view1['camera_pose'] # (B, 4, 4)
+    gt_pose2to1 = torch.inverse(view1['camera_pose']) @ view2['camera_pose'] # (B, 4, 4)
+
+    gt_pose1to2_reshape = torch.zeros(abs_pose1.shape[0], 12, device=abs_pose1.device)
+    gt_pose1to2_reshape[:, :9] = gt_pose1to2[:, :3, :3].reshape(abs_pose1.shape[0], 9)
+    gt_pose1to2_reshape[:, 9:] = gt_pose1to2[:, :3, 3]
+
+    gt_pose2to1_reshape = torch.zeros(abs_pose2.shape[0], 12, device=abs_pose2.device)
+    gt_pose2to1_reshape[:, :9] = gt_pose2to1[:, :3, :3].reshape(abs_pose2.shape[0], 9)
+    gt_pose2to1_reshape[:, 9:] = gt_pose2to1[:, :3, 3]
+
+    return gt_pose1to2_reshape, gt_pose2to1_reshape
+
+
 class Reloc3rWithDiffusionHead(nn.Module):
     def __init__(self,):
         super().__init__()
@@ -115,9 +135,59 @@ class Reloc3rWithDiffusionHead(nn.Module):
     
 
     def forward(self, view1, view2):
+        """
+        Compute the diffusion loss for training
+        """
         # Embed image features
         out1, out2 = self.backbone(view1, view2)
         image_features1, image_features2 = out1["features"], out2["features"]
 
-        raise NotImplementedError("Loss computation not implemented")
+        # Compute gt_poses
+        gt_pose1to2, gt_pose2to1 = compute_gt_poses(view1, view2)
 
+        # Compute loss
+        loss1 = self.compute_loss(image_features1, gt_pose1to2)
+        loss2 = self.compute_loss(image_features2, gt_pose2to1)
+
+        return loss1 + loss2
+    
+
+    def sample_pose(self, view1, view2, num_steps: int = 50):
+        """
+        Sample a pose from using the learned velocity model.
+        Here we only sample the pose2to1
+        """
+        # Embed image features ahead
+        out1, out2 = self.backbone(view1, view2)
+        image_features1, image_features2 = out1["features"], out2["features"]
+
+        # Define diffusion components
+        dt = -1.0 / num_steps
+        batch_size = image_features1.shape[0]
+        noise = torch.randn(batch_size, self.pose_dim, device=image_features2.device)
+
+        # Step function
+        def step(x_t, t):
+            """
+            :param x_t: a torch.Tensor of shape (B, 12)
+            :param t: a torch.Tensor of shape (B,). All elements should be the same (a multiple of dt)
+            """
+            # Embed timesteps
+            timesteps_embed = posemb_sincos(t, embedding_dim=self.output_dim, min_period=4e-3, max_period=4.0)
+
+            # Predict velocity
+            v_t = self.velocity_prediction(
+                image_embed=image_features2,
+                noisy_pose=x_t,
+                timesteps_embed=timesteps_embed
+            )  # (B, pose_dim)
+
+            return x_t + dt * v_t, t + dt
+        
+        # Begin sampling
+        t = torch.ones(batch_size, device=image_features2.device)
+        x_t = noise
+        for _ in range(num_steps):
+            x_t, t = step(x_t, t)
+
+        return x_t
